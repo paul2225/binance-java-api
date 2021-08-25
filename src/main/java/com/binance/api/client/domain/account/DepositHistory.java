9a7b778c16d108f4e84db31eee0ff9f4cbf59977
paul2225 <48255974+paul2225@users.noreply.github.com>
2021-08-25 14:48:30 +0300

    Update DepositHistory.java

--- src/main/java/com/binance/api/client/domain/account/DepositHistory.java
+++ src/main/java/com/binance/api/client/domain/account/DepositHistory.java
@@ -18,18 +18,6 @@ public class DepositHistory {
   @JsonProperty("depositList")
   private List<Deposit> depositList;
 
-  private boolean success;
-
-  private String msg;
-
-  public String getMsg() {
-    return msg;
-  }
-
-  public void setMsg(String msg) {
-    this.msg = msg;
-  }
-
   public List<Deposit> getDepositList() {
     return depositList;
   }
@@ -38,20 +26,10 @@ public class DepositHistory {
     this.depositList = depositList;
   }
 
-  public boolean isSuccess() {
-    return success;
-  }
-
-  public void setSuccess(boolean success) {
-    this.success = success;
-  }
-
   @Override
   public String toString() {
     return new ToStringBuilder(this, BinanceApiConstants.TO_STRING_BUILDER_STYLE)
         .append("depositList", depositList)
-        .append("success", success)
-        .append("msg", msg)
         .toString();
   }
 }
