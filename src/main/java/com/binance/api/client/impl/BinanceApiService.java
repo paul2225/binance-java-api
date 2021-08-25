b0653d6ceaca98dd02f7a8b224f72c7805ff2b21
paul2225 <48255974+paul2225@users.noreply.github.com>
2021-08-25 14:20:24 +0300

    Update BinanceApiService.java

--- src/main/java/com/binance/api/client/impl/BinanceApiService.java
+++ src/main/java/com/binance/api/client/impl/BinanceApiService.java
@@ -158,7 +158,7 @@ public interface BinanceApiService {
 
 
     @Headers(BinanceApiConstants.ENDPOINT_SECURITY_TYPE_SIGNED_HEADER)
-    @GET("/sapi/v1/depositHistory.html")
+    @GET("/sapi/v1/capital/deposit/hisrec")
     Call<DepositHistory> getDepositHistory(@Query("asset") String asset, @Query("recvWindow") Long recvWindow, @Query("timestamp") Long timestamp);
 
     @Headers(BinanceApiConstants.ENDPOINT_SECURITY_TYPE_SIGNED_HEADER)
