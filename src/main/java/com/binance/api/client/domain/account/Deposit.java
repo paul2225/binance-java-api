7bc28f8ece71de79634d344d0cf8c54aafe97915
paul2225 <48255974+paul2225@users.noreply.github.com>
2021-08-25 14:56:37 +0300

    Update Deposit.java

--- src/main/java/com/binance/api/client/domain/account/Deposit.java
+++ src/main/java/com/binance/api/client/domain/account/Deposit.java
@@ -1,11 +1,22 @@
 package com.binance.api.client.domain.account;
 
+import com.binance.api.client.ToStringBuilder;
 import com.binance.api.client.constant.BinanceApiConstants;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.apache.commons.lang3.builder.ToStringBuilder;
+
 
 /**
  * A deposit that was done to a Binance account.
+ * *          "amount":"0.00999800",
+ *  *         "coin":"PAXG",
+ *  *         "network":"ETH",
+ *  *         "status":1,
+ *  *         "address":"0x788cabe9236ce061e5a892e1a59395a81fc8d62c",
+ *  *         "addressTag":"",
+ *  *         "txId":"0xaad4654a3234aa6118af9b4b335f5ae81c360b2394721c019b5d1e75328b09f3",
+ *  *         "insertTime":1599621997000,
+ *  *         "transferType":0,
+ *  *         "confirmTimes":"12/12"
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Deposit {
@@ -17,26 +28,79 @@ public class Deposit {
 
   /**
    * Symbol.
-   * "amount":"0.00999800",
-   *         "coin":"PAXG",
-   *         "network":"ETH",
-   *         "status":1,
-   *         "address":"0x788cabe9236ce061e5a892e1a59395a81fc8d62c",
-   *         "addressTag":"",
-   *         "txId":"0xaad4654a3234aa6118af9b4b335f5ae81c360b2394721c019b5d1e75328b09f3",
-   *         "insertTime":1599621997000,
-   *         "transferType":0,
-   *         "confirmTimes":"12/12"
    */
   private String coin;
+
   private String network;
-  private int status;
+  /**
+   * (0:pending,1:success)
+   */
+  private Integer status;
   private String address;
   private String addressTag;
+  /**
+   * Transaction id
+   */
   private String txId;
-  private long insertTime;
-  private int transferType;
+
+  /**
+   * Deposit time.
+   */
+  private String insertTime;
+  private Integer transferType;
   private String confirmTimes;
+
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
+
+  public Integer getTransferType() {
+    return transferType;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getAddressTag() {
+    return addressTag;
+  }
+
+  public String getCoin() {
+    return coin;
+  }
+
+  public String getConfirmTimes() {
+    return confirmTimes;
+  }
+
+  public String getNetwork() {
+    return network;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public void setAddressTag(String addressTag) {
+    this.addressTag = addressTag;
+  }
+
+  public void setCoin(String coin) {
+    this.coin = coin;
+  }
+
+  public void setConfirmTimes(String confirmTimes) {
+    this.confirmTimes = confirmTimes;
+  }
+
+  public void setNetwork(String network) {
+    this.network = network;
+  }
+
+  public void setTransferType(Integer transferType) {
+    this.transferType = transferType;
+  }
 
   public String getAmount() {
     return amount;
@@ -46,44 +110,20 @@ public class Deposit {
     this.amount = amount;
   }
 
-  public String getCoin() {
+  public String getAsset() {
     return coin;
   }
 
-  public void setCoin(String coin) {
-    this.coin = coin;
+  public void setAsset(String asset) {
+    this.coin = asset;
   }
 
-  public String getNetwork() {
-    return network;
+  public String getInsertTime() {
+    return insertTime;
   }
 
-  public void setNetwork(String network) {
-    this.network = network;
-  }
-
-  public int getStatus() {
-    return status;
-  }
-
-  public void setStatus(int status) {
-    this.status = status;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
-  public String getAddressTag() {
-    return addressTag;
-  }
-
-  public void setAddressTag(String addressTag) {
-    this.addressTag = addressTag;
+  public void setInsertTime(String insertTime) {
+    this.insertTime = insertTime;
   }
 
   public String getTxId() {
@@ -94,42 +134,22 @@ public class Deposit {
     this.txId = txId;
   }
 
-  public long getInsertTime() {
-    return insertTime;
+  public int getStatus() {
+    return status;
   }
 
-  public void setInsertTime(long insertTime) {
-    this.insertTime = insertTime;
-  }
-
-  public int getTransferType() {
-    return transferType;
-  }
-
-  public void setTransferType(int transferType) {
-    this.transferType = transferType;
-  }
-
-  public String getConfirmTimes() {
-    return confirmTimes;
-  }
-
-  public void setConfirmTimes(String confirmTimes) {
-    this.confirmTimes = confirmTimes;
+  public void setStatus(int status) {
+    this.status = status;
   }
 
   @Override
   public String toString() {
-    return new ToStringBuilder(this, BinanceApiConstants.TO_STRING_BUILDER_STYLE)
+    return new ToStringBuilder()
         .append("amount", amount)
-        .append("coin", coin)
-      .append("network", network)
-      .append("status", status)
-      .append("address", address)
-        .append("addressTag", addressTag)
+        .append("asset", coin)
         .append("insertTime", insertTime)
-        .append("transferType", transferType)
-      .append("confirmTimes", confirmTimes)
+        .append("txId", txId)
+        .append("status", status)
         .toString();
   }
 }
