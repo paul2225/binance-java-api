fdaede4110f16758b5842ef0e8edbc2acacc3dbb
paul2225 <48255974+paul2225@users.noreply.github.com>
2021-09-04 17:46:56 +0300

    Update BinanceApiAsyncRestClientImpl.java

--- src/main/java/com/binance/api/client/impl/BinanceApiAsyncRestClientImpl.java
+++ src/main/java/com/binance/api/client/impl/BinanceApiAsyncRestClientImpl.java
@@ -212,7 +212,7 @@ public class BinanceApiAsyncRestClientImpl implements BinanceApiAsyncRestClient 
   @Override
   public void withdraw(String asset, String address, String amount, String name, String addressTag, BinanceApiCallback<WithdrawResult> callback) {
     binanceApiService.withdraw(asset, null, null, address, addressTag, amount, null, name,
-				BinanceApiConstants.DEFAULT_RECEIVING_WINDOW, getTimeStamp())
+				BinanceApiConstants.DEFAULT_RECEIVING_WINDOW, System.currentTimeMillis())
         .enqueue(new BinanceApiCallbackAdapter<>(callback));
   }
 
